59ed96dfef39b640ef39dc9b4e643cec37fdf11c
pensivetraveler <pensivetraveler@gmail.com>
2024-09-27 18:34:40 +0900

    refactor: nav aconfig remove annotation

--- application/config/extra/admin_nav_config.php
+++ application/config/extra/admin_nav_config.php
@@ -2,6 +2,6 @@
 
 $config['admin_nav_config_loaded'] = true;
 
-//$config['admin_menu_top'] = [];
-//
-//$config['admin_menu_side'] = [];
+$config['admin_menu_top'] = [];
+
+$config['admin_menu_side'] = [];
